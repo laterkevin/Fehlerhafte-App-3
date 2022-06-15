6e1762730b6918c292515a7b76760d285c2da68a
André Wagner <100126751+andreFdroid@users.noreply.github.com>
2022-06-15 08:48:03 +0200

    init

---
+++ app/src/main/java/com/syntax_institut/fehlerhafteapp/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.syntax_institut.fehlerhafteapp
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    // Countdownvariablen
+    private lateinit var countDownTimer: CountDownTimer
+    private var countNr: Int = 6
+
+    /**
+     * Die Lifecycle onCreate Methode
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        countdown()
+    }
+
+    /**
+     * Die Countdown Methode
+     */
+    private fun countdown() {
+        countDownTimer = object : CountDownTimer(5000, 1000) {
+            override fun onTick(p0: Long) {
+                countNr--
+                saveNumbers(countNr)
+                findViewById<TextView>(R.id.countDownTimer).text = countNr.toString()
+            }
+
+            override fun onFinish() {
+                findViewById<TextView>(R.id.countDownTimer).visibility = View.GONE
+                findViewById<TextView>(R.id.tv_app_startet).visibility = View.GONE
+                findViewById<TextView>(R.id.tv_funktioniert).visibility = View.VISIBLE
+            }
+        }.start()
+    }
+
+    /**
+     * Verändere nichts über dieser Zeile!
+     */
+
+    // Das Array in dem die Nummern gespeichert werden
+    private val numbers = arrayOfNulls<Int>(4)
+
+    // Die Variable die den Index speichert
+    private var arrayIndexCounter = 0
+
+    /**
+     * speichert die Countdown- Nummern in einem array
+     */
+    private fun saveNumbers(nr: Int) {
+        numbers[arrayIndexCounter] = nr
+        arrayIndexCounter++
+    }
+}
